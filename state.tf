50e735b8400b4771f33061dd0234327622fe9b00
Deepthi <dpdevops6@gmail.com>
2024-03-04 16:47:43 -0500

    Terraform

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-statefile-v562"
-    key    = "aws-parameters/terraform.tf.state"
+    key    = "aws-parameters/terraform.tfstate"
     region = "us-east-1"
   }
 }
